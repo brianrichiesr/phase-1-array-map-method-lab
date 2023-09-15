3bbd51328725e1f195eb8982e2d23c8b2c4b394e
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-15 14:15:11 -0700

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,20 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  /* Map through the global array 'tutorials' */
+  let result = tutorials.map(each => {
+    /* Split 'each' iteration and assign the array to a variable, then map through the array */
+    let sentence = each.split(' ').map(item => {
+      /* Reassign the value of 'item' to the first letter of 'item' capitalized plus the remainder of 'item' */
+      item = item[0].toUpperCase() + item.slice(1);
+      /* Return the capitalized 'item' */
+      return item;
+    });
+    /* Return the value of 'sentence' joined back into a sentence as 'each' */
+    return sentence.join(' ');
+
+  })
+  /* Return the new array of capitalized titles */
+  return result;
+
 }
